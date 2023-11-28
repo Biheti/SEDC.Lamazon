121c0a67f26d3156cdef8477fe19add251672de9
biljana <biljanaslavkovska13@gmail.com>
2023-11-28 19:25:19 +0100

    sedc.Lamazon

---
+++ SEDC.Lamazon.Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace SEDC.Lamazon.Domain.Entities;
+
+public class Product : BaseEntity
+{
+    //blob - binary large object
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int ImageUrl { get; set; }
+    public decimal Price { get; set; }
+
+    public int ProductCategoryId { get; set; }
+    public ProductCategory ProductCategory { get; set; }
+
+}
